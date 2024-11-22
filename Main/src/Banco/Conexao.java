2f377cb6067720cbf8f03f04ed8449becb71c8fb
Lucas Duarte <lucasduarte.tec@gmail.com>
2024-11-22 06:32:37 -0300

    atualização de repositorio

--- Main/src/Banco/Conexao.java
+++ Main/src/Banco/Conexao.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/MetalurgiaDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "kashi125";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
